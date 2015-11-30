30476d2fe102fae44c2d0d93d7d01b1a29d50ea8
Kike Gamboa <kikeremiau@gmail.com>
2015-11-30 17:02:42 -0600

    Initial Commit

---
+++ Pizzas Kiketelo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pizzas Kiketelo
+//
+//  Created by Telecomunicaciones Abiertas de México on 11/30/15.
+//  Copyright © 2015 Telecomunicaciones Abiertas de México. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
